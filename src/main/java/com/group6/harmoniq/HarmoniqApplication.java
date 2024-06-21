16f76dcb8e8344efa5a89be6a8e36d8869876795
LanceWWS <yerkinboranbaev19@gmail.com>
2024-06-21 15:29:10 -0700

    first commit

---
+++ src/main/java/com/group6/harmoniq/HarmoniqApplication.java
@@ -0,0 +1,13 @@
+package com.group6.harmoniq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmoniqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmoniqApplication.class, args);
+	}
+
+}
