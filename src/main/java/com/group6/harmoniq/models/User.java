218d642ef3b30825490852c9b54a39894716f90f
mononomori <miguel.a.cannuli@gmail.com>
2024-06-25 23:05:43 -0700

    feat: added User class and OAuthController for pulling populating user-table with user-data

---
+++ src/main/java/com/group6/harmoniq/models/User.java
@@ -0,0 +1,118 @@
+package com.group6.harmoniq.models;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Enforce ID uniqueness and ensures every user has Spotify user ID
+    @Column(unique = true, nullable = false)
+    private String spotifyId;
+    
+    private String displayName;
+
+    private String email;
+
+    // Allows access to user data
+    private String accessToken;
+
+    // Refreshes token without requiring user reauthorization every hour
+    private String refreshToken;
+
+    // How long access token is valid
+    private int expiresIn;
+    
+    @Column(updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt = new Date();
+
+    // Constructors
+    public User() {}
+
+    public User(String spotifyId, String displayName, String email, String accessToken, String refreshToken, int expiresIn) {
+        this.spotifyId = spotifyId;
+        this.displayName = displayName;
+        this.email = email;
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+        this.expiresIn = expiresIn;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSpotifyId() {
+        return spotifyId;
+    }
+
+    public void setSpotifyId(String spotifyId) {
+        this.spotifyId = spotifyId;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public int getExpiresIn() {
+        return expiresIn;
+    }
+
+    public void setExpiresIn(int expiresIn) {
+        this.expiresIn = expiresIn;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+
+    
+    
+}
