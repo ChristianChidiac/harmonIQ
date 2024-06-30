2d1a3a1a0701ff6d53927a361c2b6189d43a162e
Baljeet Singh <22825252+sbaljeet13@users.noreply.github.com>
2024-06-30 16:24:34 -0700

    Add User Model

---
+++ src/main/java/com/group6/harmoniq/models/User.java
@@ -0,0 +1,68 @@
+package com.group6.harmoniq.models;
+
+public class User {
+    private String displayName;
+    private String email;
+    private String externalSpotifyUrl;
+    private int followers;
+    private String id;
+    private String imageUrl;
+
+    public User(String displayName, String email, String externalSpotifyUrl, int followers, String id,
+            String imageUrl) {
+        this.displayName = displayName;
+        this.email = email;
+        this.externalSpotifyUrl = externalSpotifyUrl;
+        this.followers = followers;
+        this.id = id;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getExternalSpotifyUrl() {
+        return externalSpotifyUrl;
+    }
+
+    public void setExternalSpotifyUrl(String externalSpotifyUrl) {
+        this.externalSpotifyUrl = externalSpotifyUrl;
+    }
+
+    public int getFollowers() {
+        return followers;
+    }
+
+    public void setFollowers(int followers) {
+        this.followers = followers;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+    
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
