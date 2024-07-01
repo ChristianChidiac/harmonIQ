585c4400cb4e6c69ce658613c1572c73329a746e
Baljeet Singh <22825252+sbaljeet13@users.noreply.github.com>
2024-06-30 21:48:41 -0700

    Add `/profile` endpoint

--- src/main/java/com/group6/harmoniq/controllers/UserController.java
+++ src/main/java/com/group6/harmoniq/controllers/UserController.java
@@ -1,8 +1,24 @@
 package com.group6.harmoniq.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.group6.harmoniq.models.User;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class UserController {
+    @GetMapping("/profile")
+    public String getProfile(Model model, HttpServletRequest request, HttpSession session) {
+        User user = new User();
+        user.setDisplayName("Baljeeeeeeet");
+        user.setImageUrl("https://i.scdn.co/image/ab6775700000ee850ff63f304a0cacb691011c34");
+        user.setFollowers(10);
+        model.addAttribute("user", user);
+        return "profile";
+    }
     
 }
