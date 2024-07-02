7a7c08c7ff9999936d44da6d0166310178ada631
Christian Chidiac <chrischidiac1@gmail.com>
2024-07-02 12:28:13 -0700

    Fix: removed the second /callback GetMapping in UserController, and capitalized all quiz files

--- src/main/java/com/group6/harmoniq/controllers/SpotifyController.java
+++ src/main/java/com/group6/harmoniq/controllers/SpotifyController.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CookieValue;
