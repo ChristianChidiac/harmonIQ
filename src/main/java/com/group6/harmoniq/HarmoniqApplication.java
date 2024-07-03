dcd0a3697cbf60fdfc4714199c4a7fedd1f253f5
ChristianChidiac <chrischidiac1@gmail.com>
2024-07-02 23:11:49 -0700

    Added dotenv import

--- src/main/java/com/group6/harmoniq/HarmoniqApplication.java
+++ src/main/java/com/group6/harmoniq/HarmoniqApplication.java
@@ -2,6 +2,7 @@ package com.group6.harmoniq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class HarmoniqApplication {
