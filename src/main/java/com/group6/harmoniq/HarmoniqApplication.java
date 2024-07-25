09f5e1b08765984b023fcb11ed377a302a5a05c5
Christian Chidiac <chrischidiac1@gmail.com>
2024-07-24 20:00:11 -0700

    Added collaborative playlist

--- src/main/java/com/group6/harmoniq/HarmoniqApplication.java
+++ src/main/java/com/group6/harmoniq/HarmoniqApplication.java
@@ -3,10 +3,16 @@ package com.group6.harmoniq;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class HarmoniqApplication {
 
 	public static void main(String[] args) {
+				Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> {
+		System.setProperty(entry.getKey(), entry.getValue());
+	   });
 		SpringApplication.run(HarmoniqApplication.class, args);
 	}
 
