c2e2bd8d8d1c2e2653836b1257f4bc05e3e412c5
Yerkin <122606509+LanceWWS@users.noreply.github.com>
2024-07-29 18:50:30 -0700

    modified tests according to new Album Quiz

--- src/test/java/com/group6/harmoniq/models/QuizTest.java
+++ src/test/java/com/group6/harmoniq/models/QuizTest.java
@@ -5,18 +5,22 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuizTest {
 
-    private Quiz quiz;
-    private static final String question_path = "images/albumcovers/Elephant.png";
-    private static final String answer = "The White Stripes";
+    private QuizQuestion quiz;
+    private static final Long id = 1L;
+    private static final String question_url = "https://i.scdn.co/image/ab67616d0000b273ce4f1737bc8a646c8c4bd25a";
+    private static final String answer = "Bohemian Rhapsody";
     private static final String option1 = "Post Malone";
     private static final String option2 = "Led Zeppelin";
     private static final String option3 = "The Rolling Stones";
 
     @BeforeEach
     public void setUp() {
-        quiz = new Quiz(question_path, answer, option1, option2, option3);
+        quiz = new QuizQuestion(id, question_url, answer, option1, option2, option3);
     }
 
     @Test
@@ -41,12 +45,12 @@ public class QuizTest {
 
     @Test
     void testGetQuestionPath() {
-        assertEquals(question_path, quiz.getQuestionPath()); 
+        assertEquals(question_url, quiz.getQuestionUrl()); 
     }
 
     @Test
     void testSetAnswer() {
-        String newAnswer = "Taylor Swift";
+        String newAnswer = "The Beatles";
         quiz.setAnswer(newAnswer);
         assertEquals(newAnswer, quiz.getAnswer());
     }
@@ -67,15 +71,15 @@ public class QuizTest {
 
     @Test
     void testSetOption3() {
-        String newOption3 = "The Beatles";
+        String newOption3 = "Taylor Swift" ;
         quiz.setOption3(newOption3);
         assertEquals(newOption3, quiz.getOption3());
     }
 
     @Test
     void testSetQuestionPath() {
-        String newQuestionPath = "images/albumcovers/SpeakNow.png";
-        quiz.setQuestionPath(newQuestionPath);
-        assertEquals(newQuestionPath, quiz.getQuestionPath());
+        String newQuestionUrl = "https://i.scdn.co/image/ab6761610000e5ebe9348cc01ff5d55971b22433";
+        quiz.setQuestionUrl(newQuestionUrl);
+        assertEquals(newQuestionUrl, quiz.getQuestionUrl());
     }
 }
